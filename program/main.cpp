0c561ce620b71756f926ad7d10ada182db17c3ee
davisvielma <davisvielma@gmail.com>
2022-12-05 15:28:29 -0400

    Agregamos la funcion insertar en la clase Cola

--- program/main.cpp
+++ program/main.cpp
@@ -12,6 +12,14 @@ int main(int argc, char const *argv[]) {
 	} else {
 		cout << "La cola tiene valores" << endl;
 	}
+
+	cola->insertarElemento(45);
+
+	if(cola->vacio()) {
+		cout << "La cola esta vacia" << endl;
+	} else {
+		cout << "La cola tiene el valor: " << cola->verInicio() << endl;
+	}
 	
 	return 0;
 }
